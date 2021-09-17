6be823e6a25bd2a8a0470bd725539587ecca51b7
Pooya Jaferian <pooya.j@gmail.com>
2021-09-17 13:26:41 -0700

    Example for easier testing (#74)
    * webpack example
    
    * add yarn dev
    
    Co-authored-by: Julio <julio.farah@gmail.com>

---
+++ example/webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    index: './src/index.js',
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    static: path.resolve(__dirname),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Development',
+    }),
+  ],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+};
