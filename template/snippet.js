0b9f8f9d2d753120d69c3efb9ce437854bbf789e
Seth Silesky <5115498+silesky@users.noreply.github.com>
2023-06-22 13:31:30 -0500

    stabilize minified variables (#132)

--- template/snippet.js
+++ template/snippet.js
@@ -46,19 +46,20 @@
   // for methods in Analytics.js so that you never have to wait
   // for it to load to actually record data. The `method` is
   // stored as the first argument, so we can replay the data.
-  analytics.factory = function(method){
+  analytics.factory = function(e){
     return function(){
       if (window.analytics.initialized) {
         // Sometimes users assigned analytics to a variable before analytics is done loading, resulting in a stale reference.
         // If so, proxy any calls to the 'real' analytics instance.
-        return window.analytics[method].apply(window.analytics, arguments);
+        return window.analytics[e].apply(window.analytics, arguments);
       }
       var args = Array.prototype.slice.call(arguments);
-      args.unshift(method);
+      args.unshift(e);
       analytics.push(args);
       return analytics;
     };
   };
+
 
   // For each of our methods, generate a queueing stub.
   for (var i = 0; i < analytics.methods.length; i++) {
@@ -70,14 +71,14 @@
   // and that will be sure to only ever load it once.
   analytics.load = function(key, options){
     // Create an async script element based on your key.
-    var script = document.createElement('script');
-    script.type = 'text/javascript';
-    script.async = true;
-    script.src = "https://<%= settings.host %><%= settings.ajsPath %>";
+    var t = document.createElement('script');
+    t.type = 'text/javascript';
+    t.async = true;
+    t.src = "https://<%= settings.host %><%= settings.ajsPath %>";
 
     // Insert our script next to the first script element.
     var first = document.getElementsByTagName('script')[0];
-    first.parentNode.insertBefore(script, first);
+    first.parentNode.insertBefore(t, first);
     analytics._loadOptions = options;
   };
   analytics._writeKey = '<%= settings.apiKey %>';
